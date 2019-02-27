a71e0e73923cd0b71c925960ea646b9c35c407b2
Emily Spieler <emilyspieler1@gmail.com>
2019-02-26 21:11:58 -0600

    email change

--- action_page.php
+++ action_page.php
@@ -3,7 +3,7 @@
   if (isset($_REQUEST['femail']))  {
 
   //Email information
-  $admin_email = "emily@introchicago.live";
+  $admin_email = "emily@emilyspielerphotography.com";
   $fname = $_REQUEST['fname'];
   $femail = $_REQUEST['femail'];
   $fphone = $_REQUEST['fphone'];
