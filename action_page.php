a1b181756f4bea947e8778c30a02b0191d0927f6
Emily Spieler <emilyspieler1@gmail.com>
2019-02-26 20:58:55 -0600

    php fix

--- action_page.php
+++ action_page.php
@@ -1,6 +1,6 @@
 <?php
 //if "email" variable is filled out, send email
-  if (isset($_REQUEST['email']))  {
+  if (isset($_REQUEST['femail']))  {
 
   //Email information
   $admin_email = "emily@emilyspielerphotography.com";
@@ -23,34 +23,7 @@
   else  {
 ?>
 
-<form method="post" name="myForm">
-  <label for="fname">Name</label>
-    <input type="text" name="fname"><br>
-
-    <label for="femail">Email</label>
-    <input type="email" name="femail" id="email"><br>
-
-    <label for="fphone">Phone</label>
-    <input type="tel" id="phone" name="fphone"
-    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}">
-    <span class="note">Format: 123-456-7890</span><br>
-
-    <label for="fnumber">Number of party members</label>
-    <input type="number" name="fnumber"><br>
-
-    <label for="fdate">Desired date of reservation:</label>
-    <input type="date" name="fdate"><br>
-
-    <label for="ftime">Desired time of reservation</label>
-    <input type="time" id="appt" name="ftime"
-    min="9:00" max="18:00"><br>
-    <span class="note">Restaurant hours are 9am to 6pm</span><br>
-
-    <label for="fmessage">Any additional Information</label>
-    <input type="text" name="fmessage"><br>
-    <br>
-  <input type="submit" value="Submit">
-</form>
+<h1>there was an error please go back and try again<h1>
 
 
 <?php
