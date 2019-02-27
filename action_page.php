e4f8354dfeca1c71c0ac2acef485f3550a8c2a90
Emily Spieler <emilyspieler1@gmail.com>
2019-02-26 20:55:11 -0600

    add php email form

--- action_page.php
+++ action_page.php
@@ -1,19 +1,58 @@
 <?php
-if(isset($_POST['submit'])){
-    $to = "emily@emilyspielerphotography.com"; // this is your Email address
-    $from = $_POST['femail']; // this is the sender's Email address
-    $fname = $_POST['fname'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $fname . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['fmessage'] . $_POST['ftime'] . $_POST['date'] ;
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['email']))  {
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $fname . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
+  //Email information
+  $admin_email = "emily@emilyspielerphotography.com";
+  $fname = $_REQUEST['fname'];
+  $fname = $_REQUEST['femail'];
+  $fname = $_REQUEST['fphone'];
+  $fname = $_REQUEST['fnumber'];
+  $fname = $_REQUEST['fdate'];
+  $fname = $_REQUEST['ftime'];
+  $fname = $_REQUEST['fmessage'];
+
+  //send email
+  mail($admin_email, "$subject", $comment, "From:" . $email);
+
+  //Email response
+  echo "Thank you for contacting us! someone will be in touch within 24 hours to confirm your reservation";
+  }
+
+  //if "email" variable is not filled out, display the form
+  else  {
+?>
+
+<form method="post" name="myForm">
+  <label for="fname">Name</label>
+    <input type="text" name="fname"><br>
+
+    <label for="femail">Email</label>
+    <input type="email" name="femail" id="email"><br>
+
+    <label for="fphone">Phone</label>
+    <input type="tel" id="phone" name="fphone"
+    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}">
+    <span class="note">Format: 123-456-7890</span><br>
+
+    <label for="fnumber">Number of party members</label>
+    <input type="number" name="fnumber"><br>
+
+    <label for="fdate">Desired date of reservation:</label>
+    <input type="date" name="fdate"><br>
+
+    <label for="ftime">Desired time of reservation</label>
+    <input type="time" id="appt" name="ftime"
+    min="9:00" max="18:00"><br>
+    <span class="note">Restaurant hours are 9am to 6pm</span><br>
+
+    <label for="fmessage">Any additional Information</label>
+    <input type="text" name="fmessage"><br>
+    <br>
+  <input type="submit" value="Submit">
+</form>
+
+
+<?php
+  }
 ?>
